a324bcda67d7d8e08165506a2c099e820cb9c962
BimRai <115172913+BimRai@users.noreply.github.com>
2023-11-14 15:08:12 +0000

    Add files via upload

---
+++ show_picker.py
@@ -0,0 +1,6 @@
+from random import choice
+#list of shows
+shows = [ 'My Hero Academia', 'Jujutsu Kaisen', 'Vigilante (K-Drama)', 'Breaking Bad', 'Loki', 'Attack On Titan','SAS: Worlds Toughest', 'Your Name', 'Squid Game', ]
+
+#print show
+print(choice(shows) + "!")
